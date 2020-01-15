adb200c5ae4836ecc1f0859ae0c9357383c19dd0
joaozinho22 <joao.silva.008@acad.pucrs.br>
2020-01-15 16:53:43 -0300

    Updates

---
+++ precache-manifest.ddb6406995c715ab287fc537dc9dba7a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "108137655e7dafc62c8a90378ad7e551",
+    "url": "/monstersrolodex/index.html"
+  },
+  {
+    "revision": "5a629c97ef423d425c23",
+    "url": "/monstersrolodex/static/css/main.467cf0aa.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/monstersrolodex/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monstersrolodex/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5a629c97ef423d425c23",
+    "url": "/monstersrolodex/static/js/main.f098447b.chunk.js"
+  },
+  {
+    "revision": "ca2709bd436d545a2d66",
+    "url": "/monstersrolodex/static/js/runtime-main.257d90d8.js"
+  }
+]);
